096445748130cc7cadf196c59d541640aed3aa87
abc <abc@def.com>
2024-04-24 22:25:50 +0800

    add code

--- fast_writer3.go
+++ fast_writer3.go
@@ -2,17 +2,19 @@ package fw
 
 import (
 	"os"
-	"runtime"
 	"sync"
-	"time"
 )
 
 type FastWriter3 struct {
-	f       *os.File
-	mux     sync.Mutex
-	bufSize int
-	buf     []byte
-	current int
+	fileMux          sync.Mutex
+	f                *os.File
+	mux              sync.Mutex
+	notifyWriterChan chan struct{}
+	bufAvailableCond *sync.Cond
+	bufSize          int
+	buf              []byte
+	current          int
+	writerWorking    bool
 }
 
 func NewFastWriter3(fileName string) (*FastWriter3, error) {
@@ -27,6 +29,8 @@ func NewFastWriter3(fileName string) (*FastWriter3, error) {
 }
 
 func (w *FastWriter3) init(fileName string) error {
+	w.bufAvailableCond = sync.NewCond(&w.mux)
+	w.notifyWriterChan = make(chan struct{}, 1)
 	w.buf = make([]byte, w.bufSize)
 	var err error
 	w.f, err = os.OpenFile(fileName, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
@@ -41,36 +45,50 @@ func (w *FastWriter3) writeLoop() {
 	buf := make([]byte, w.bufSize)
 	for {
 		w.mux.Lock()
-		if w.current == 0 {
+		for w.current == 0 {
+			w.writerWorking = false
 			w.mux.Unlock()
-			time.Sleep(time.Millisecond)
-			continue
+			select {
+			case <-w.notifyWriterChan:
+			}
+			w.mux.Lock()
 		}
 		buf, w.buf = w.buf, buf
 		contentSize := w.current
 		w.current = 0
+		w.writerWorking = true
 		w.mux.Unlock()
-		_, err := w.f.Write(buf[0:contentSize])
-		if err != nil {
-			break
-		}
+		w.bufAvailableCond.Broadcast()
+		w.writeToFile(buf[0:contentSize])
 	}
 }
 
-func (w *FastWriter3) Write(p []byte) (n int, err error) {
-	for {
-		w.mux.Lock()
-		if len(p)+w.current > len(w.buf) {
-			w.mux.Unlock()
-			runtime.Gosched()
-			continue
-		}
-		copy(w.buf[w.current:], p)
-		w.current += len(p)
-		w.mux.Unlock()
-		break
+func (w *FastWriter3) Write(b []byte) (n int, err error) {
+	l := len(b)
+	if l > w.bufSize {
+		return w.writeToFile(b)
 	}
-	return len(p), nil
+	w.mux.Lock()
+	for l+w.current > len(w.buf) {
+		w.bufAvailableCond.Wait()
+	}
+	copy(w.buf[w.current:], b)
+	w.current += l
+	writerWorking := w.writerWorking
+	w.mux.Unlock()
+	if !writerWorking {
+		select {
+		case w.notifyWriterChan <- struct{}{}:
+		default:
+		}
+	}
+	return l, nil
+}
+
+func (w *FastWriter3) writeToFile(b []byte) (n int, err error) {
+	w.fileMux.Lock()
+	defer w.fileMux.Unlock()
+	return w.f.Write(b)
 }
 
 func (w *FastWriter3) Close() error {
