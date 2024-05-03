81ec8332d6d3ab54d746fd7cbf1c5fd33f0c4278
abc <abc@def.com>
2024-05-03 22:58:41 +0800

    add code

--- fast_writer_test.go
+++ fast_writer_test.go
@@ -33,3 +33,55 @@ func TestFastWriter_ConcurrentWrite(t *testing.T) {
 	require.Equal(t, contentLen*concurrentCount, buf.Len())
 	w.Close()
 }
+
+func TestFastWriter_SmallBuffer(t *testing.T) {
+	defer goleak.VerifyNone(t)
+	buf := bytes.NewBuffer(nil)
+	w, err := NewFastWriter(&FastWriterConfig{
+		BufferSize: 1,
+		Writer:     buf,
+	})
+	require.NoError(t, err)
+	var wg sync.WaitGroup
+	concurrentCount := runtime.NumCPU() * 3
+	wg.Add(concurrentCount)
+	const contentLen = 10000
+	for i := 0; i < concurrentCount; i++ {
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 10000; i++ {
+				w.Write([]byte("a"))
+			}
+		}()
+	}
+	wg.Wait()
+	w.Flush()
+	require.Equal(t, contentLen*concurrentCount, buf.Len())
+	w.Close()
+}
+
+func TestFastWriter_SmallBuffer2(t *testing.T) {
+	defer goleak.VerifyNone(t)
+	buf := bytes.NewBuffer(nil)
+	w, err := NewFastWriter(&FastWriterConfig{
+		BufferSize: 1,
+		Writer:     buf,
+	})
+	require.NoError(t, err)
+	var wg sync.WaitGroup
+	concurrentCount := runtime.NumCPU() * 3
+	wg.Add(concurrentCount)
+	const contentLen = 10000
+	for i := 0; i < concurrentCount; i++ {
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 10000; i++ {
+				w.Write([]byte("aaa"))
+			}
+		}()
+	}
+	wg.Wait()
+	w.Flush()
+	require.Equal(t, 3*contentLen*concurrentCount, buf.Len())
+	w.Close()
+}
